28b0c8a89d00930fa9bd411b902900e9b2245f89
Sterling <sterlingduncan9@gmail.com>
2023-08-11 10:36:05 -0400

    changed stuff in movie display

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,30 +1,35 @@
-import React from 'react'; 
-import styled from 'styled-components'
-import {Title, MovieImage} from './styles/App.styled'
+import React from "react";
+import styled from "styled-components";
+import { Title, MovieImage } from "./styles/App.styled";
+import {
+  DisplayContainer,
+  DisplayZ,
+  LittleDiv,
+  BigDiv,
+} from "./styles/Display.styled";
 
+function MovieDisplay({ movie }) {
+  const loaded = () => {
+    return (
+      <DisplayZ>
+        <BigDiv>
+          <MovieImage src={movie.Poster} alt={movie.Title} />
+        </BigDiv>
+        <LittleDiv>
+          <Title>{movie.Title}</Title>
+          <h2>{movie.Genre}</h2>
+          <h2>{movie.Year}</h2>
+          {/* <h2>{movie.Ratings}</h2> */}
+        </LittleDiv>
+      </DisplayZ>
+    );
+  };
 
+  const loading = () => {
+    return <h1> No Movie to Display</h1>;
+  };
 
-
-
-
-function MovieDisplay({movie}) {
-
-    const loaded = () => {
-        return (
-            <>
-                <Title>{movie.Title}</Title>
-                <h2>{movie.Genre}</h2>
-                <MovieImage src={movie.Poster} alt={movie.Title} />
-                <h2>{movie.Year}</h2>
-            </>
-        );
-    };
-
-    const loading = () => {
-        return <h1> No Movie to Display</h1>
-    }
-
-  return movie? loaded() : loading()
+  return movie ? loaded() : loading();
 }
 
-export default MovieDisplay
+export default MovieDisplay;

9adbf11b9b438b712ef7f622e7f6b95b59afb893
Sterling <sterlingduncan9@gmail.com>
2023-08-11 10:36:17 -0400

    change style for display

---
+++ src/components/styles/Display.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+export const DisplayContainer = styled.div`
+  display: flex;
+  border: 4px solid yellow;
+  height: 200px;
+  width: 200px;
+  margin-left: 80%;
+`;
+export const DisplayZ = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const LittleDiv = styled.div`
+  flex: 10%;
+`;
+
+export const BigDiv = styled.div`
+  flex: 35%;
+  text-align: right;
+`;
